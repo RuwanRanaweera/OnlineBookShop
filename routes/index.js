8141d96f61670314d61429455b6565d011c2fcd6
hasthiyaIT <hasthiyait@gmail.com>
2022-04-11 08:50:57 +0530

    booking

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const routes = require("express").Router();
+
+// const UserRoutes = require("./User");
+// const ProjectRoutes = require("./project");
+
+// routes.use("/user", UserRoutes);
+// routes.use("/project", ProjectRoutes);
+
+module.exports = routes;
